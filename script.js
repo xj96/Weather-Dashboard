94f663b0bb6a04ccb932c8d79a185b709ee28876
Xavier Jackson <66346130+xj96@users.noreply.github.com>
2022-03-21 21:49:48 -0400

    Update script.js
    Removed comments

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV i
-// WHEN I view the UV i
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day day5 that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-// WHEN I open the weather dashboard
-// THEN I am presented with the last searched city day5
 
 // Weather API
 const apiKey = "c972785c6cce92cd4ef6abedf6d41f9a";
